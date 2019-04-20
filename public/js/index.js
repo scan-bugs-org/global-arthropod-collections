8a9a11af55cbf11d573dbc24e64e4596307c9a26
Evin Dunn <evindunn14@gmail.com>
2019-04-19 19:55:25 -0700

    Adds templating for eventual data api

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,11 @@
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 mapboxgl.accessToken = "pk.eyJ1IjoiZXZpbmR1bm4iLCJhIjoiY2ptMWhoNDV3MTRtcjN2cnZiejc5MDZ6NiJ9.5kJNTKKD9-Y6KrU1Sf0rNw";
 
-/**
- * Main function for the page
- */
 function main() {
   let map = new mapboxgl.Map({
     container: "map",
-    style: 'mapbox://styles/mapbox/outdoors-v10',
+    style: "mapbox://styles/mapbox/outdoors-v11",
     center: [-111.7, 35.2],
-    zoom: 10
+    zoom: 8
   });
 }
 
--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+#!/usr/bin/env/node
+
 const express = require("express");
 const logger = require("morgan");
+const nunjucks = require("nunjucks");
 const path = require("path");
+const http = require("http");
 const util = require("util");
+
+const indexRouter = require("./routes/index");
 
 const NODE_ENV = process.env["NODE_ENV"] || "development";
 const PORT = process.env["PORT"] || 8080;
 
 let app = express();
+app.set("port", PORT);
 app.use(logger(NODE_ENV == "development" ? "dev" : "tiny"));
-app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(PORT, () => {
-  console.log(util.format("Server listening on port %d...", PORT))
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: NODE_ENV == "development"
 });
+
+app.set("view engine", nunjucks);
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+let server = http.createServer(app);
+server.listen(PORT);
+server.on("listening", () => {
+  console.log(util.format("Server listening on port %d...", PORT));
+});
+server.on("error", console.error);
