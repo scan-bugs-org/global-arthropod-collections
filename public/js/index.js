354f44e6dc0ab69821a86bb85a4b8f5595fb54b9
Evin Dunn <evindunn14@gmail.com>
2020-05-19 12:42:53 -0700

    Make markers blue

--- public/js/index.js
+++ public/js/index.js
@@ -148,7 +148,7 @@ function getMarkerRadius(map, feature) {
  */
 function getMarkerStyle(map, layer) {
   return {
-    fillColor: "darkgreen",
+    fillColor: "#00336e",
     fillOpacity: 0.6,
     stroke: false,
     radius: getMarkerRadius(map, layer.feature),
