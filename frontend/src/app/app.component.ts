f84f19b21c83ee2a9e1e56c12343ed983de83bfa
Evin Dunn <evindunn14@gmail.com>
2020-12-07 10:32:41 -0700

    Sets the app title

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-    styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'frontend';
+    constructor(private readonly title: Title) {
+        this.title.setTitle("Global Arthropod Collections");
+    }
 }
