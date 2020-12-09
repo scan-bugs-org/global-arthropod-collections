ed282886ab106cb299e2ba9951b7c8943eb484c6
Evin Dunn <evindunn14@gmail.com>
2020-12-08 20:23:51 -0700

    Can properly update collections

--- frontend/src/app/collection/collection.component.ts
+++ frontend/src/app/collection/collection.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CollectionService } from '../services/collection.service';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Collection } from '../services/dto/collection.dto';
 import { AlertService } from '../services/alert.service';
-import { FormControl, FormGroup } from '@angular/forms';
-
-type TableData = { name: string, value: any };
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-collection',
@@ -20,15 +18,15 @@ export class CollectionComponent implements OnInit {
 
     public nameControl = new FormControl('');
     public codeControl = new FormControl('');
-    public sizeControl = new FormControl(0);
-    public tierControl = new FormControl(4);
+    public sizeControl = new FormControl(0, [Validators.min(0)]);
+    public tierControl = new FormControl(4, [Validators.min(0), Validators.max(4)]);
     public urlControl = new FormControl('');
     public iDigBioControl = new FormControl(false);
 
     public provinceControl = new FormControl('');
     public countryControl = new FormControl('');
-    public latControl = new FormControl(0);
-    public lngControl = new FormControl(0);
+    public latControl = new FormControl(0, [Validators.min(-90.0), Validators.max(90.0)]);
+    public lngControl = new FormControl(0, [Validators.min(-180.0), Validators.max(180.0)]);
 
     public form = new FormGroup({
         'name': this.nameControl,
@@ -76,7 +74,23 @@ export class CollectionComponent implements OnInit {
     }
 
     applyEdits() {
-        this.isEditing = false;
+        if (this.collection) {
+            this.isEditing = false;
+            this.collections.updateByID(this.collection._id, this.form.value)
+                .pipe(
+                    catchError(() => {
+                        this.alert.showError('Error updating institution');
+                        return of(null);
+                    })
+                )
+                .subscribe((collection) => {
+                    if (collection) {
+                        this.alert.showMessage('Collection updated successfully');
+                        this.collection = collection;
+                        this.resetValues(this.collection);
+                    }
+                });
+        }
     }
 
     cancelEdits() {
