f467bfac97e118912459bab42c66da27969b9388
Evin Dunn <evindunn14@gmail.com>
2020-12-23 13:29:34 -0600

    Use a dev token

--- frontend/src/environments/environment.common.ts
+++ frontend/src/environments/environment.common.ts
@@ -1,7 +1,7 @@
 export class CommonEnvironment {
     static production = false;
     static apiUrl = "http://127.0.0.1:8080/api/v1";
-    static mapboxToken = 'pk.eyJ1IjoiZXZpbmR1bm4iLCJhIjoiY2thY3M3cXMwMWM4bzJ1cnliNnlkNHV2MiJ9.Twln5aH0wWGw9iKJoPE2Kg';
+    static mapboxToken = 'pk.eyJ1IjoiZXZpbmR1bm4iLCJhIjoiY2thY3J2bTdiMDNhdzJ5cXc2c2w2NWJlaCJ9.7vnANHAtTNT2eqhwWexOKQ';
 
     static tilesUrl = `https://tiles.bug-collections.org/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${CommonEnvironment.mapboxToken}`;
     static institutionUrl = `${CommonEnvironment.apiUrl}/institutions`;
