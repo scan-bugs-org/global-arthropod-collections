73257033032d4eb26a53f28980cef035cd1724b5
Evin Dunn <evindunn14@gmail.com>
2021-01-04 17:41:58 -0700

    Creates a 3-package monorepo

---
+++ packages/common/dist/upload/header-mapping-input.dto.d.ts
@@ -0,0 +1,18 @@
+export interface HeaderMappingInputInterface {
+    institutionName: string;
+    institutionCode: string;
+    collectionCode: string;
+    size: string;
+    country: string;
+    state: string;
+    collectionName: string;
+    latitude: string;
+    longitude: string;
+    tier: string;
+    url: string;
+    inIdigbio: string;
+    scanExists: string;
+    scanType: string;
+    gbifExists: string;
+    gbifDate: string;
+}
