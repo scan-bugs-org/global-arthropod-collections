296b7c3c4ece4c87efe2dba141a8fff9acbc6c94
Evin Dunn <evindunn14@gmail.com>
2021-01-07 03:05:36 -0700

    Make urls more dynamic in frontend

--- packages/frontend/src/environments/environment.common.ts
+++ packages/frontend/src/environments/environment.common.ts
@@ -3,9 +3,23 @@ export class CommonEnvironment {
     static apiUrl = "http://127.0.0.1:8080/api/v1";
     static mapboxToken = 'pk.eyJ1IjoiZXZpbmR1bm4iLCJhIjoiY2thY3J2bTdiMDNhdzJ5cXc2c2w2NWJlaCJ9.7vnANHAtTNT2eqhwWexOKQ';
 
-    static tilesUrl = `http://127.0.0.1:8082/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${CommonEnvironment.mapboxToken}`;
-    static institutionUrl = `${CommonEnvironment.apiUrl}/institutions`;
-    static collectionUrl = `${CommonEnvironment.apiUrl}/collections`;
-    static uploadUrl = `${CommonEnvironment.apiUrl}/uploads`;
-    static loginUrl = `${CommonEnvironment.apiUrl}/login`;
+    static get tilesUrl() {
+        return `http://127.0.0.1:8082/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${this.mapboxToken}`;
+    }
+
+    static get institutionUrl() {
+        return `${this.apiUrl}/institutions`;
+    }
+
+    static get collectionUrl() {
+        return `${this.apiUrl}/collections`;
+    }
+
+    static get uploadUrl() {
+        return `${this.apiUrl}/uploads`;
+    }
+
+    static get loginUrl() {
+        return `${this.apiUrl}/login`;
+    }
 }
