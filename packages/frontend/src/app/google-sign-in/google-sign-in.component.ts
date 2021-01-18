9af7eced016909729e23a90e18ec273a7057f2f6
Evin Dunn <evindunn14@gmail.com>
2021-01-18 11:06:34 -0700

    Adds a google sign in btn

--- packages/frontend/src/app/google-sign-in/google-sign-in.component.ts
+++ packages/frontend/src/app/google-sign-in/google-sign-in.component.ts
@@ -25,7 +25,11 @@ export class GoogleSignInComponent implements OnInit {
 
     onSignIn() {
         this.authInstance?.signIn().then((user: GoogleUser) => {
-            console.log(user);
+            const profile = user.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
         }).catch((e) => {
             this.alert.showError(JSON.stringify(e));
