059e6aeee6f7ca53b3c1a40c4effbbfd5ff893ee
Evin Dunn <evindunn14@gmail.com>
2021-01-18 17:51:48 -0700

    Don't commit common/dist

--- packages/common/dist/user-output.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export interface UserOutputInterface {
-    given_name: string;
-    family_name: string;
-    picture: string;
-    fullName: string;
-}
--- packages/common/dist/user-output.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=user-output.dto.js.map
