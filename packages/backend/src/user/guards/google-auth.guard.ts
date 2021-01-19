7f22a3bb3c7de5b08558ac439499963c5e185d98
Evin Dunn <evindunn14@gmail.com>
2021-01-19 00:09:22 -0700

    Adds login menu

--- packages/backend/src/user/guards/google-auth.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { AuthGuard, PassportStrategy } from "@nestjs/passport";
-
-@Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') {}
